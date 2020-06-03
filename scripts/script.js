207adaa31b30d4a7cb38d696fa5021e88a7c1d05
Pete Gilbert <pete.gilbert16@gmail.com>
2020-06-03 17:06:59 -0400

    Added settings menu and updated styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,13 @@
-// 0 = North  1 = East  2 = South  = 3 West
-$(document).ready(function(){
     // level will track number of turns
     let level = 1;
-    let timer = 60;
+    let timer = $('input[name=gameLength]').val();
+    let userVolume = $('input[name=volume]').val() / 100
 
-    const reset = function(startingTime){
+    //This function is just housekeeping and reseting values.
+    const reset = function(){
         countdownFunction()
         level = 1;
-        timer = startingTime;
+        timer = $('input[name=gameLength]').val();
         $(".turns").html("")
         startingValue = createStartingValue()
         startingDirection = translateToDirection(startingValue);
@@ -15,20 +15,23 @@ $(document).ready(function(){
         turnValue = createTurns(level);
         answer = findAnswer(startingValue, turnValue);
         $(".gameOver").addClass("hidden");
-        $('.time').html(`${timer}`);
+        $(".settings").addClass("hidden");
+        $('.time').html($('input[name=gameLength]').val());
         document.getElementById("backgroundMusic").play();
     }
 
-    $(".play").on("click", function(){
-        //Removing Instructions Section
-        $(".howToPlay").remove();
-        //Showing Game Section
-        $(".game").toggleClass("hidden");
-        
-        
-        //RESETING DEFAULT VALUES
-        reset(60);
-    })
+    //This is a timer function it counts down every second and runs gameOver function when finished
+    $(".time").html(`${timer}`);
+    const countdownFunction = function(){
+        const countdown = setInterval(() => {
+            timer--;
+            $('.time').html(`${timer}`);
+            if (timer <= 0) {
+                clearInterval(countdown)
+                gameOver(level);
+            }
+        }, 1000)
+    }
 
     // This function creates a random number between 400-403 to represent starting direction. Starting at 400 to prevent negative numbers while being divisable by 4 for the translate to direction function later.
     const createStartingValue = function(){
@@ -54,11 +57,10 @@ $(document).ready(function(){
     }
     let startingDirection = translateToDirection(startingValue);
 
-
+    //This function creates a number that we can combine with the starting value and then % 4 to return the correct ending direction
     const createTurns = function(level){
-        //turnValue creates a number that we can combine with the starting value and then % 4 to return the correct ending direction
         let turnValue = 0
-        // turnWord will add the turns to the DOM so the player can read them as left or right
+        // turnWord will be added the turns to the DOM so the player can read them as left or right
         let turnWord = ""
         for (let i = 1; i < level; i++){
             //math.random will return 0 or 1
@@ -78,10 +80,39 @@ $(document).ready(function(){
     }
     let turnValue = createTurns(level);
 
+    //This function returns a value between 0-3 that is comparable to the values of the radio buttons (the correct answer in the game)
     const findAnswer = function(startingValue, TurnValue){
         return ((startingValue + TurnValue) % 4)
     }
     let answer = findAnswer(startingValue, turnValue);
+
+    //This function hides the game and brings up the gameOver screen
+    const gameOver = function(score){
+        $(".game").addClass("hidden")
+        $(".score").html(score)
+        document.getElementById("backgroundMusic").pause();
+        document.getElementById("gameOver").play();
+        $(".gameOver").removeClass("hidden")
+    }
+
+
+
+$(document).ready(function(){
+
+    $(".play").on("click", function(){
+        //Removing Instructions Section
+        $(".howToPlay").remove();
+        //Showing Game Section
+        $(".game").toggleClass("hidden");
+        //RESETING DEFAULT VALUES
+        reset();
+
+        //Adjusting volume of all elements
+        document.getElementById("incorrect").volume = userVolume 
+        document.getElementById("correct").volume = userVolume
+        document.getElementById("backgroundMusic").volume = userVolume
+        console.log(userVolume) 
+    })
 
     $(".submit").on("click", function(){
         let userAnswer = ($('input[name=answer]:checked').val())
@@ -103,31 +134,13 @@ $(document).ready(function(){
         turnValue = createTurns(level);
         answer = findAnswer(startingValue, turnValue);
 
-
-        console.log(`Cheat: ${translateToDirection(answer)}`);
+        //THIS IS LEFT IN SO THAT WE CAN CHEAT AS THE GAME IS ACTUALLY QUITE DIFFICULT!
+        console.log(`Cheater's answer: ${translateToDirection(answer)}`);
     })
 
-    const gameOver = function(score){
-        $(".game").addClass("hidden")
-        $(".score").html(score)
-        document.getElementById("backgroundMusic").pause();
-        document.getElementById("gameOver").play();
-        $(".gameOver").removeClass("hidden")
-    }
-
-    //TIMER 
-    $(".time").html(`${timer}`);
-    const countdownFunction = function(){
-        const countdown = setInterval(() => {
-            timer--;
-            $('.time').html(`${timer}`);
-            if (timer <= 0) {
-                clearInterval(countdown)
-                gameOver(level);
-            }
-        }, 1000)
-    }
-
-
-
+    $(".settingsButton").on("click", function(){
+        $(".howToPlay").remove();
+        $(".settings").toggleClass("hidden");
+        $(".gameOver").addClass("hidden");
+    })
 })
